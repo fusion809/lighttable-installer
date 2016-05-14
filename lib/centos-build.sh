188fe5f77cb214ce73bbd22a32c6c78b5d17067a
fusion809 <brentonhorne77@gmail.com>
2016-05-14 16:03:58 +1000

    Expanding

--- lib/centos-build.sh
+++ lib/centos-build.sh
@@ -6,5 +6,7 @@ function centos-build {
   curl -sL https://rpm.nodesource.com/setup_6.x | sudo bash -     # Get the latest version of Node.js
   sudo yum install -y unzip nodejs-devel gcc gcc-c++ make \
     glibc-devel git-core gtk2-devel python libgnome-keyring-devel
-  lighttable-build
+  LightTable-build
 }
+
+export -f centos-build
