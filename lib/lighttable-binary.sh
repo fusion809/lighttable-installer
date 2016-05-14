61c8b3a9b3bd9a407fb1087bef41bc88c6a0b605
fusion809 <brentonhorne77@gmail.com>
2016-05-14 15:01:19 +1000

    Expanding

--- lib/lighttable-binary.sh
+++ lib/lighttable-binary.sh
@@ -7,20 +7,32 @@ function lighttable-binary {
   if ! [[ -n $SRC_DEST ]]; then
     SRC_DEST=$PRG
   fi
+
+  if ! [[ -d /tmp/lighttable ]]; then
+    git clone https://aur.archlinux.org/lighttable.git /tmp/lighttable
+  else
+    cd /tmp/lighttable
+    git pull origin master
+  fi
+  pkgname=lighttable
+  pkgver=$(sed -n 's/pkgver=//p' /tmp/lighttable/PKGBUILD)
+  _destdir="/opt/LightTable"
+
+  wget -cqO- https://github.com/LightTable/LightTable/releases/download/$ver/$pkgname-$pkgver-linux.tar.gz | tar xz -C $SRC_DEST
+
   wget -cqO- https://github.com/fusion809/lighttable-installer/raw/master/lighttable > $SRC_DEST/lighttable
   wget -cqO- https://github.com/fusion809/lighttable-installer/raw/master/lighttable.png > $SRC_DEST/lighttable.png
   wget -cqO- https://github.com/fusion809/lighttable-installer/raw/master/lighttable.desktop > $SRC_DEST/lighttable.desktop
+  wget -cqO- https://github.com/fusion809/lighttable-installer/raw/master/LICENSE > $SRC_DEST/LICENSE
+
   sudo install -Dm 755 "$SRC_DEST/lighttable" "/usr/bin/lighttable"
+  sudo install -dm755 "/usr/share/licenses/lighttable"
+  sudo install -Dm 644 "$SRC_DEST/LICENSE" "/usr/share/licenses/lighttable/LICENSE"
 
-  install -dm755 "$pkgdir/usr/share/licenses/lighttable"
-  install -Dm 644 "$srcdir/LICENSE" "$pkgdir/usr/share/licenses/lighttable/LICENSE"
+  sudo install -dm755 $_destdir
+  sudo mv $SRC_DEST/lighttable-${pkgver//_/-}-linux/* $_destdir
 
-  _destdir="$pkgdir/opt/LightTable"
-
-  install -dm755 $_destdir
-  mv $srcdir/lighttable-${pkgver//_/-}-linux/* $_destdir
-
-  install -Dm 644 "$srcdir/lighttable.desktop" "$pkgdir/usr/share/applications/lighttable.desktop"
-  install -Dm 644 "$srcdir/lighttable.png" "$pkgdir/usr/share/pixmaps/lighttable.png"
+  sudo install -Dm 644 "$SRC_DEST/lighttable.desktop" "/usr/share/applications/lighttable.desktop"
+  sudo install -Dm 644 "$SRC_DEST/lighttable.png" "/usr/share/pixmaps/lighttable.png"
 
 }
