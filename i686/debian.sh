683150f86094f099696db301466dd121cb734f4e
fusion809 <brentonhorne77@gmail.com>
2016-05-14 16:24:23 +1000

    Expanding

--- i686/debian.sh
+++ i686/debian.sh
@@ -10,10 +10,11 @@ if ! [[ -f /usr/lib/libudev.so.0 ]]; then
   rm libudev0_175-7.2_$ARC.deb
 fi
 
+sudo apt-get install -y wget default-jre
 
 if [[ $preference == "A" ]]; then
 
-  sudo apt-get install -y wget alsa-lib nss libgnome-keyring-dev fakeroot libgtk2.0-dev
+  sudo apt-get install -y alsa-lib nss libgnome-keyring-dev fakeroot libgtk2.0-dev
   lighttable-binary
 
 else
