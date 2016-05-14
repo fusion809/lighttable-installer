7e594a77da957ffd495ca47d895c04e7abf82433
fusion809 <brentonhorne77@gmail.com>
2016-05-14 16:00:36 +1000

    Expanding

--- x86_64/centos.sh
+++ x86_64/centos.sh
@@ -2,17 +2,18 @@
 printf "Do you want to install Brackets from A) a binary package (faster) or would you prefer B) a source install (slower)? [A/B; B is default] "
 read preference
 
+if ! [[ -f /usr/lib/libudev.so.0 ]]; then
+  curl -sL ftp://195.220.108.108/linux/centos/6.7/updates/x86_64/Packages/libudev-147-2.63.el6_7.1.x86_64.rpm > /tmp/libudev-147-2.63.el6_7.1.x86_64.rpm
+  if ! `comex bsdtar`; then
+    sudo yum install -y bsdtar
+  fi
+  bsdtar -xf /tmp/libudev-147-2.63.el6_7.1.x86_64.rpm -C /tmp
+  sudo cp -a /tmp/lib64/libudev.so.0* /usr/lib/
+fi
+
 if [[ $preference == "A" ]]; then
 
   sudo yum install -y wget alsa-lib nss libgnome-keyring-devel gtk2-devel
-  if ! [[ -f /usr/lib/libudev.so.0 ]]; then
-    curl -sL ftp://195.220.108.108/linux/centos/6.7/updates/x86_64/Packages/libudev-147-2.63.el6_7.1.x86_64.rpm > /tmp/libudev-147-2.63.el6_7.1.x86_64.rpm
-    if ! `comex bsdtar`; then
-      sudo yum install -y bsdtar
-    fi
-    bsdtar -xf /tmp/libudev-147-2.63.el6_7.1.x86_64.rpm -C /tmp
-    sudo cp -a /tmp/lib64/libudev.so.0* /usr/lib/
-  fi
   lighttable-binary
 
 else
