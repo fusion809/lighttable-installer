f3aea0f0a0fa40b38f13bfed92537ecb04b67039
fusion809 <brentonhorne77@gmail.com>
2016-05-17 14:54:51 +1000

    [aur] Expanding

--- lib/build/aur.sh
+++ lib/build/aur.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-. ./lib/build/lighttable.sh
 . ./lib/test.sh
 
 function lightin {
-  cd /tmp/lighttable
+  cd /tmp/PKGBUILDs/lighttable
   makepkg -si --noconfirm
   cd .
 }
@@ -18,31 +17,77 @@ function lightaur {
   export AUR=https://aur.archlinux.org/cgit/aur.git/snapshot/
   export GIT=https://aur.archlinux.org/
 
-  if comex yaourt; then                    # Install with yaourt if possible
+  printf "Would you prefer to install LightTable from the AUR or from my PKGBUILDs GitHub repository? [AUR/GitHub/?; GitHub is the default] "
+  read choice
+
+  if [[ $choice == "?" ]]; then
+
+    printf 'If you opt to install LightTable from the AUR, the AUR helper `yaourt` will be used to install it. This means that when updates become available from the AUR you will be able to install them (along with packages updates from the pacman repositories and other AUR packages you have installed) by running `yaourt -Syua`.\nMy PKGBUILD for LightTable should be up-to-date and uses a desktop configuration file with MimeType support for a wider variety of different file formats than that provided by the AUR.'
+
+    printf "Would you prefer to install LightTable from the AUR or from my PKGBUILDs GitHub repository? [AUR/GitHub; GitHub is the default] "
+    read choice
+
+  fi
+
+  if [[ $choice == "AUR" ]]; then
+
+    if comex yaourt; then                    # Install with yaourt if possible
+
+      printf "Great you have yaourt already installed!"
+
+    elif comex git; then                      # Install with git and makepkg otherwise
+
+      printf "Ah, it seems you do not have yaourt installed, so I'm about to install it for you!"
+
+      git clone $GIT/package-query.git /tmp/package-query
+      git clone $GIT/yaourt.git /tmp/yaourt
+
+      cd /tmp/package-query && makepkg -si --noconfirm
+      cd /tmp/yaourt && makepkg -si --noconfirm
+
+    elif comex curl; then                     # Install with curl and makepkg otherwise
+
+      curl -sL $AUR/package-query.tar.gz | tar xz -C /tmp
+      curl -sL $AUR/yaourt.tar.gz | tar xz -C /tmp
+      cd /tmp/package-query && makepkg -si --noconfirm
+      cd /tmp/yaourt && makepkg -si --noconfirm
+
+    elif comex wget; then                     # Install with wget and makepkg otherwise
+
+      wget -cqO- $AUR/package-query.tar.gz | tar xz -C /tmp
+      wget -cqO- $AUR/yaourt.tar.gz | tar xz -C /tmp
+      cd /tmp/package-query && makepkg -si --noconfirm
+      cd /tmp/yaourt && makepkg -si --noconfirm
+
+    else                                      # Install curl and install with curl and makepkg otherwise
+
+      sudo pacman -S curl --noconfirm
+      curl -sL $AUR/package-query.tar.gz | tar xz -C /tmp
+      curl -sL $AUR/yaourt.tar.gz | tar xz -C /tmp
+      cd /tmp/package-query && makepkg -si --noconfirm
+      cd /tmp/yaourt && makepkg -si --noconfirm
+
+    fi
 
     yaourt -S lighttable --noconfirm
 
-  elif comex git; then                      # Install with git and makepkg otherwise
+  else # GitHub is the default
 
-    git clone $GIT/lighttable.git /tmp/lighttable
+    if comex git; then
+
+      git clone https://github.com/fusion809/PKGBUILDs.git /tmp/PKGBUILDs
+
+    elif comex curl; then
+
+      curl -sL https://github.com/fusion809/PKGBUILDs/archive/master.tar.gz | tar xz --transform=s/PKGBUILDs-master/PKGBUILDs -C /tmp
+
+    elif comex wget; then
+
+      wget -cqO- https://github.com/fusion809/PKGBUILDs/archive/master.tar.gz | tar xz --transform=s/PKGBUILDs-master/PKGBUILDs -C /tmp
+
+    fi
+
     lightin
-
-  elif comex curl; then                     # Install with curl and makepkg otherwise
-
-    curl -sL $AUR/lighttable.tar.gz | tar xz -C /tmp
-    lightin
-
-  elif comex wget; then                     # Install with wget and makepkg otherwise
-
-    wget -cqO- $AUR/lighttable.tar.gz | tar xz -C /tmp
-    lightin
-
-  else                                      # Install curl and install with curl and makepkg otherwise
-
-    sudo pacman -S curl --noconfirm
-    curl -sL $AUR/lighttable.tar.gz | tar xz -C /tmp
-    lightin
-
   fi
 }
 
