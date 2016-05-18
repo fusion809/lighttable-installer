0eae4377d33836722a1b7fcb1c4d6739fbcda387
fusion809 <brentonhorne77@gmail.com>
2016-05-19 05:42:27 +1000

    Fixing local install

--- lib/dest.sh
+++ lib/dest.sh
@@ -3,17 +3,21 @@ function dest {
   printf "Where do you want to store the downloaded files? [Leave empty for $1] "
   read SRC_DEST
 
-  printf "Do you want to install LightTable locally or system-wide? [local/system; default is system] "
+  printf "Do you want to install LightTable locally or system-wide? [local/system; default is local] "
   read DEST_TYPE
 
   if [[ $DEST_TYPE == "local" ]]; then
-    printf "Where do you want to install LightTable? [default is $SRC_DEST/builds/$pkgname-$pkgver-linux] "
+    printf "Where do you want to install LightTable? [default is $HOME/.local] "
     read INST_DEST
 
     if [[ -n $INST_DEST ]]; then
-      INST_DEST=$SRC_DEST/builds/$pkgname-$pkgver-linux
+      INST_DEST=$HOME/.local
     fi
 
+    if ! [[ -d $INST_DEST ]]; then
+      mkdir -p $INST_DEST
+    fi
+    
     export INST_DEST
   fi
 
