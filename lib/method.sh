e50346bc1c5cb4433d4e4c7a79e37494607c1fa0
fusion809 <brentonhorne77@gmail.com>
2016-05-18 22:47:38 +1000

    Adding extra info to method

--- lib/method.sh
+++ lib/method.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function method {
-  printf "Would you rather install LightTable from: A) a binary tarball (faster) or B) from source (slower)? [A/B; B is the default] "
+  printf "Would you rather install LightTable from: A) a binary tarball (faster, also safer) or B) from source (slower)? [A/B; B is the default] "
   read method
 }
 
